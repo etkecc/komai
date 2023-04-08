0xDEADCADE/nheko

Add rainfall effect
This is a proof-of-concept example of inplementing a msgtype not found
in the spec.

src/CommandCompleter.cpp
@@ -87,6 +87,10 @@ CommandCompleter::data(const QModelIndex &index, int role) const
                 return QStringLiteral("/confetti ");
             case RainbowConfetti:
                 return QStringLiteral("/rainbowconfetti ");
+            case Rainfall:
+                return QStringLiteral("/rainfall ");
+            case RainbowRain:
+                return QStringLiteral("/rainbowrain ");
             case Goto:
                 return QStringLiteral("/goto ");
             case ConvertToDm:
@@ -156,6 +160,10 @@ CommandCompleter::data(const QModelIndex &index, int role) const
                 return tr("/confetti [message]");
             case RainbowConfetti:
                 return tr("/rainbowconfetti [message]");
+            case Rainfall:
+                return QStringLiteral("/rainfall [message]");
+            case RainbowRain:
+                return QStringLiteral("/rainbowrain [message]");
             case Goto:
                 return tr("/goto <message reference>");
             case ConvertToDm:
@@ -225,6 +233,10 @@ CommandCompleter::data(const QModelIndex &index, int role) const
                 return tr("Send a message with confetti.");
             case RainbowConfetti:
                 return tr("Send a message in rainbow colors with confetti.");
+            case Rainfall:
+                return tr("Send a message with rain.");
+            case RainbowRain:
+                return tr("Send a message in rainbow colors with rain.");
             case Goto:
                 return tr("Go to a specific message using an event id, index or matrix: link");
             case ConvertToDm:

etkecc/komai

src/CommandCompleter.cpp
@@ -87,6 +87,10 @@ CommandCompleter::data(const QModelIndex &index, int role) const
                 return QStringLiteral("/confetti ");
             case RainbowConfetti:
                 return QStringLiteral("/rainbowconfetti ");
+            case Rainfall:
+                return QStringLiteral("/rainfall ");
+            case RainbowRain:
+                return QStringLiteral("/rainbowrain ");
             case Goto:
                 return QStringLiteral("/goto ");
             case ConvertToDm:
@@ -156,6 +160,10 @@ CommandCompleter::data(const QModelIndex &index, int role) const
                 return tr("/confetti [message]");
             case RainbowConfetti:
                 return tr("/rainbowconfetti [message]");
+            case Rainfall:
+                return tr("/rainfall [message]");
+            case RainbowRain:
+                return tr("/rainbowrain [message]");
             case Goto:
                 return tr("/goto <message reference>");
             case ConvertToDm:
@@ -225,6 +233,10 @@ CommandCompleter::data(const QModelIndex &index, int role) const
                 return tr("Send a message with confetti.");
             case RainbowConfetti:
                 return tr("Send a message in rainbow colors with confetti.");
+            case Rainfall:
+                return tr("Send a message with rain.");
+            case RainbowRain:
+                return tr("Send a message in rainbow colors with rain.");
             case Goto:
                 return tr("Go to a specific message using an event id, index or matrix: link");
             case ConvertToDm:
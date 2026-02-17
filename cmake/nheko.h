badd0f2ede10c9fc977956c17b469c90cd597e4b
Slavi Pantaleev <slavi@devture.com>
2026-02-17 15:43:47 +0200

    Override upstream version with Komai version
    Replace nheko version in CMakeLists.txt with Komai's own version and
    inject upstream attribution metadata (version + commit hash) into the
    C++ build via cmake/nheko.h.

--- cmake/nheko.h
+++ cmake/nheko.h
@@ -1,5 +1,6 @@
 namespace nheko {
 inline constexpr auto version          = "${PROJECT_VERSION}";
+inline constexpr auto upstream_version = "abb2325a995f936081219f402339fc8e0a661ac1 (abb2325a)";
 inline constexpr auto build_os         = "${CMAKE_HOST_SYSTEM_NAME}";
 inline constexpr auto enable_debug_log = ${SPDLOG_DEBUG_ON};
 }

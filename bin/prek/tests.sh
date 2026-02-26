db0d289bccb285818b370ddf5439d78e5b7d7dac
Slavi Pantaleev <slavi@devture.com>
2026-02-26 07:16:11 +0200

    Make pre-commit tests hook sandbox-safe for just tempdir

--- bin/prek/tests.sh
+++ bin/prek/tests.sh
@@ -3,4 +3,8 @@
 set -eu
 
 repo_root="$(CDPATH= cd -- "$(dirname -- "$0")/../.." && pwd)"
+
+# Some sandboxed environments provide non-writable XDG runtime dirs (e.g. /run/user/*).
+# Force just's temp script directory into /tmp for this hook run.
+JUST_TEMPDIR="${JUST_TEMPDIR:-/tmp/just}" \
 just --justfile "$repo_root/justfile" test
